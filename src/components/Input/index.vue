42ec6ee36a1e24774258918a9418647133fda23b
kerry-okpere <kerryokpere@gmail.com>
2021-11-09 09:25:13 +0100

    wip: create input component

---
+++ src/components/Input/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <input :type="type" :value="modelValue" @update:modelValue="$emit('update:modelValue', $event.target.value)" >
+</template>
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      default: 'text',
+      validator: function(value) {
+        return ['text', 'email', 'tel' ].includes(value)
+      }
+    }
+  },
+  setup() {
+    
+  },
+  emits: ['update:modelValue']
+}
+</script>
