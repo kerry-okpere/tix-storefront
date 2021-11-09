47b258b9f8e3cd955e9d4c14256ddfa89b0a74f5
kerry-okpere <kerryokpere@gmail.com>
2021-11-09 09:25:44 +0100

    wip: create search component

---
+++ src/components/Search/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <input type="search" :value="modelValue" @update:modelValue="$emit('update:modelValue', $event.target.value)" >
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: String,
+      required: true
+    }
+  },
+  setup() {
+    
+  },
+  emits: ['update:modelValue']
+}
+</script>
