fa04c947080e97cb844adb3796ca554c20e46f88
kerry-okpere <kerryokpere@gmail.com>
2021-11-09 09:26:28 +0100

    add routes

--- src/router/index.js
+++ src/router/index.js
@@ -4,16 +4,25 @@ import Home from '../views/Home.vue'
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: { 
+      name: 'Home',
+      params: {
+        id: 'myShop'
+      }
+    }
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/:id',
+    name: 'Home',
+    component: () => import('../views/Home.vue')
+  },
+  {
+    path: '/checkout',
+    name: 'Checkout',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Checkout.vue')
   }
 ]
 
