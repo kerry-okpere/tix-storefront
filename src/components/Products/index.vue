445012c7183e61f975cfd52425d1b0327048ae07
kerry-okpere <kerryokpere@gmail.com>
2021-11-09 09:25:29 +0100

    wip: create product component

---
+++ src/components/Products/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div @click="$router.push({ path: '/', query: { product: slug }})">
+    <div>
+      <img :src="image" :alt="`${name} image`">
+    </div>
+    <div>
+      <p>{{ name }}</p>
+      <p>{{ price.currency }}{{ price.value }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    slug: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Object,
+      required: true
+    },
+    image: {
+      type: String,
+      required: true
+    }
+  },
+  setup() {
+    
+  },
+}
+</script>
