c5c4e1503d9078e5fca82d530a63b8f6384e5249
kerry-okpere <kerryokpere@gmail.com>
2021-11-11 16:25:10 +0100

    style search component

--- src/components/Search/index.vue
+++ src/components/Search/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <div>
-    <input type="search" :value="modelValue" @update:modelValue="$emit('update:modelValue', $event.target.value)" >
+  <div class="flex space-x-5 items-center shadow-md bg-white pl-6 rounded-md text-gray-400 max-w-4xl mx-auto">
+    <SearchIcon class="h-4 w-4" />
+    <input type="search" class="flex-1 self-stretch py-4 rounded-tr-md rounded-br-md focus:outline-none" 
+      placeholder="Search..." :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" >
   </div>
+  
 </template>
 <script>
+import { SearchIcon } from '@heroicons/vue/solid'
+
 export default {
   props: {
     modelValue: {
@@ -12,7 +17,9 @@ export default {
     }
   },
   setup() {
-    
+  },
+  components: {
+    SearchIcon
   },
   emits: ['update:modelValue']
 }
