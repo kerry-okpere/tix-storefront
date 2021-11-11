62245fe1f2f0b27fc90f62408ebd4cb509f71956
kerry-okpere <kerryokpere@gmail.com>
2021-11-11 21:41:32 +0100

    style cart

--- src/components/Product/index.vue
+++ src/components/Product/index.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="h-48 w-48 relative rounded-xl bg-gray-200 shadow-sm m-1" @click="$router.push({ path: '/', query: { product: slug }})">
+  <div class="h-48 w-48 relative rounded-xl bg-gray-200 shadow-sm m-1">
+  <!-- @click="$router.push({ path: '/', query: { product: slug }})" -->
     <img class="h-full w-full rounded-lg object-cover object-center hover:opacity-75 transition" :src="image" :alt="`${name} image`">
     <div class="absolute bg-white rounded-md p-2 bottom-2 w-11/12 text-sm mx-auto inset-x-0">
       <p class="truncate text-xs text-gray-500">{{ name }}</p>
       <p class="text-sm font-bold truncate text-gray-700">{{ money }}</p>
-      <div class="absolute p-1.5 rounded-full -top-3 right-3" :style="{backgroundColor: theme}" @click="$emit('add')">
+      <div class="absolute p-1.5 rounded-full -top-3 right-3" :style="{backgroundColor: theme}" @click.stop="$emit('add')">
         <PlusIcon class="h-4 w-4 text-white"/>
       </div>
     </div>
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,21 +1,88 @@
 <template>
-  <div>
-    <h1>Shopping Cart</h1>
-    <section class="grid grid-cols-1 divide-y divide-gray-200">
-      <ProductList v-for="product in products" :key="product.id" 
-        v-bind="product" :image="product.images[0]"/>
+  <div class="h-screen flex justify-center">
+    <div v-if="error || loading" class="h-screen flex justify-center items-center"> 
+      <p v-if="error">Error occured while fetching this user </p>
+      <p v-else-if="loading">loading... </p>
+    </div>
+    <section v-else class="flex-1">
+      <h1 class="text-4xl font-extrabold text-center mt-20 mb-12">Shopping Cart</h1>
+      <div v-if="$store.state.cart.length < 1" class="w-1/3 flex flex-col items-stretch mx-auto text-center py-32">
+        <p class="text-lg text-gray-700 py-8">Your Cart is Empty</p>
+        <!-- use user info for link -->
+        <Button title="Continue Shopping" @click="$router.push(`/${user.slug}`)" :theme="user.brandColor" />
+      </div>
+      
+      <div class="flex flex-col items-stretch max-w-xl mx-auto">
+        <ProductList class="" v-for="(product, index) in $store.state.cart" :key="product.id" v-bind="product" 
+        :amount="product.count" :theme="user.brandColor" :image="product.images[0]" :borderBottom="index === $store.state.cart.length-1"
+        @add="addQuanity(product)" @remove="reduceQuanity(product)" @removeItem="removeProduct(product)" />
+        <div class="flex justify-between py-12 w-full">
+          <h4 class="font-bold">Subtotal</h4>
+          <p>{{ formatter.format($store.getters.totalCart) }}</p>
+        </div>
+        <Button title="Checkout" @click="$router.push('/checkout')" :theme="user.brandColor" />
+
+        <div class="py-4 w-full text-center">
+          or <router-link :to="`/${user.slug}`" class="font-semibold " :style="{color: user.brandColor}">Continue Shopping</router-link>
+        </div>
+      </div>
     </section>
   </div>
 </template>
 <script>
 import ProductList from '@/components/ProductList/index.vue'
+import { useStore } from 'vuex'
+import { computed, reactive, toRefs } from 'vue'
+import { formatter } from "@/utils/getData.js"
+import NumericInput from '@/components/NumericInput/index.vue'
+import Button from '@/components/Button/index.vue'
 
 export default {
   setup() {
-    
+    const store = useStore() 
+    const state = reactive({
+      user: computed(() => store.state.user),
+      loading: false,
+      error: false      
+    })
+
+    const fetchUser = async () => {
+      state.loading = true
+      state.error = false
+      try {
+        await store.dispatch('GET_USER')
+      } catch (error) {
+        console.log(error)
+        state.error = true
+      }
+      state.loading = false
+    }
+    const getUser = () => {
+      if(Object.keys(store.state.user).length < 1) fetchUser()
+    }
+    const addQuanity = product => {
+      store.commit('increaseProductCount', product.id)
+    }
+    const reduceQuanity = product => {
+      store.commit('decreaseProductCount', product.id)
+    }
+    const removeProduct = product => {
+      store.commit('deleteProduct', product)
+    }
+
+    getUser()
+    return {
+      ...toRefs(state),
+      addQuanity,
+      reduceQuanity,
+      removeProduct,
+      formatter
+    }
   },
   components: {
-    ProductList
+    ProductList,
+    NumericInput,
+    Button
   }
 }
 </script>
