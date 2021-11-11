5c6b4f23c5413b6123aee4afe1d4afc7bdc1e4de
kerry-okpere <kerryokpere@gmail.com>
2021-11-12 00:46:53 +0100

    create inpute component

--- src/components/Input/index.vue
+++ src/components/Input/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <input :type="type" :value="modelValue" @update:modelValue="$emit('update:modelValue', $event.target.value)" >
+  <div class="my-2">
+    <label for="input">
+      <span class="text-sm text-gray-700">{{ label }}</span>
+      <input class="block w-full py-2 px-1 border rounded border-gray-300" :required="required" :type="type" :value="modelValue" name="input" @input="$emit('update:modelValue', $event.target.value)" >
+    </label>
+  </div>
 </template>
 <script>
 export default {
@@ -12,8 +17,15 @@ export default {
       type: String,
       default: 'text',
       validator: function(value) {
-        return ['text', 'email', 'tel' ].includes(value)
+        return ['text', 'email', 'tel', 'number' ].includes(value)
       }
+    },
+    required: {
+      type: Boolean,
+      default: true
+    },
+    label: {
+      type: String,
     }
   },
   setup() {
@@ -22,3 +34,10 @@ export default {
   emits: ['update:modelValue']
 }
 </script>
+<style>
+input[type=number]::-webkit-inner-spin-button, 
+input[type=number]::-webkit-outer-spin-button { 
+  -webkit-appearance: none; 
+  margin: 0; 
+}
+</style>
