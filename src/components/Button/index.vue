2a5d9c795c6da53d4cd5eb9a00008fcee8ae8a8b
kerry-okpere <kerryokpere@gmail.com>
2021-11-11 21:38:15 +0100

    create button component

---
+++ src/components/Button/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <button :class="['py-3 px-6 rounded font-semibold text-white hover:opacity-90 transition', {'cursor-not-allowed': disabled}]" :style="{backgroundColor: theme}" :disabled="disabled">
+    {{ title }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    theme: {
+      type: String,
+      default: '#FBBF24'
+    },
+  }
+}
+</script>
