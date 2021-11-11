82493f7274eed3b3c5b01ae6caf007fa39b5ac02
kerry-okpere <kerryokpere@gmail.com>
2021-11-11 16:24:40 +0100

    create badge component

---
+++ src/components/Badge/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex items-center space-x-3 px-4 py-2 rounded text-white" :style="{backgroundColor: theme}">
+    <ShoppingBagIcon class="h-5 w-5 " />
+    <p>{{ value }}</p>
+  </div>
+</template>
+<script>
+import { ShoppingBagIcon } from '@heroicons/vue/outline'
+
+export default {
+  props: {
+    theme: {
+      type: String,
+      default: '#FBBF24'
+    },
+    value: {
+      type: Number,
+      default: 0
+    }
+  },
+  setup() {
+    
+  },
+  components: {
+    ShoppingBagIcon
+  }
+}
+</script>
