b8b734d673736e0d83679e51952e38e7620118b5
kerry-okpere <kerryokpere@gmail.com>
2021-11-12 02:05:13 +0100

    add scss

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
 import store from './store'
+import './assets/_base.scss'
 
 createApp(App).use(store).use(router).mount('#app')
